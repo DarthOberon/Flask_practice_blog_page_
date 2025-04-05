f60ad73608f95a005e599d1dffc3a41f877d0ab2
DarthOberon <10aryansharma@gmail.com>
2025-04-05 23:56:50 +0530

    added register and login for the blog page

--- practice_1.py
+++ practice_1.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy 
+from forms import Resgistrationform, Loginform
 app =Flask(__name__)
+
+
+app.config['SECRET_KEY'] = '3dd13468875312d337e24a2350503cae'
 
 posts =[
     {
@@ -26,6 +31,18 @@ def home():
 @app.route("/about")
 def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = Resgistrationform()
+    return render_template('regster.html',title= 'Register',form=form)
+
+@app.route("/login")
+def login():
+    form = Loginform()
+    return render_template('login.html',title= 'Login',form=form)
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
