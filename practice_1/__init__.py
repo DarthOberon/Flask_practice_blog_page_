47bb71ec9ff1785a4f237cc12a09f8a66f06864b
DarthOberon <10aryansharma@gmail.com>
2025-04-12 10:53:12 +0530

    Had forgotten to add the dotenv in imports Now added!

--- practice_1/__init__.py
+++ practice_1/__init__.py
@@ -1,9 +1,11 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from dotenv import load_dotenv
 import os
 
 app =Flask(__name__)
+load_dotenv()
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
