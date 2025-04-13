c4e9a69ed76bbe505445b9ee4824317a8fe341d4
DarthOberon <10aryansharma@gmail.com>
2025-04-13 13:32:59 +0530

    Updated these files to have proper login, register and display account and ask to login before veiwing account

--- practice_1/__init__.py
+++ practice_1/__init__.py
@@ -1,6 +1,8 @@
 from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy 
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 from dotenv import load_dotenv
 import os
 
@@ -9,6 +11,8 @@ app =Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
 
 from practice_1 import routes
--- practice_1/models.py
+++ practice_1/models.py
@@ -1,11 +1,16 @@
 from datetime import datetime
-from practice_1 import db
+from practice_1 import db, login_manager
+from flask_login import UserMixin
 
-class  User(db.Model):
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id)) 
+
+class  User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(20),unique = True, nullable = False)
     email = db.Column(db.String(120),unique = True, nullable = False)
-    image_file = db.Column(db.String(20), unique = False, nullable = False, default = 'defaul.jpg')
+    image_file = db.Column(db.String(20), unique = False, nullable = False, default = 'default.jpg')
     password = db.Column(db.String(60),nullable = False)
     posts = db.relationship('Post', backref ='author', lazy = True)
 
