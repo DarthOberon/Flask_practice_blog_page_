756655ee5516f1ed956abd51d5b9599c1a529136
DarthOberon <10aryansharma@gmail.com>
2025-04-13 11:05:47 +0530

    The resgister page wasn't loading, now fixed

--- practice_1/__init__.py
+++ practice_1/__init__.py
@@ -4,8 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from dotenv import load_dotenv
 import os
 
-app =Flask(__name__)
 load_dotenv()
+app =Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
